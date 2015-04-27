df60fb9129c633822403f8e0b96a35eb09e9c7ae
Tyler Fisher <tyler@tylerjfisher.com>
2015-04-27 19:30:31 -0400

    add pym

--- jplayer_basic/js/app.js
+++ jplayer_basic/js/app.js
@@ -1,3 +1,5 @@
+var pymChild;
+
 var setAudioMedia = function() {
     $('#audio-player').jPlayer('setMedia', {
         mp3: 'https://archive.org/download/audio_poetry_90_2006/prettyhowtown.mp3'
@@ -16,4 +18,6 @@ $(document).ready(function() {
 
     $('.begin').on('click', setAudioMedia);
     $('.pause').on('click', pauseAudio);
+
+    pymChild = new pym.Child();
 });
